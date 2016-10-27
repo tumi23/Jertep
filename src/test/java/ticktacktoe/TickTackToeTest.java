76b09d5e0e1a16bc2128ef4d8fadb6b1d8b0aacc
Tumi <Tumi>
2016-10-27 16:36:05 +0000

    Implemented modify_board test

--- src/test/java/ticktacktoe/TickTackToeTest.java
+++ src/test/java/ticktacktoe/TickTackToeTest.java
@@ -9,14 +9,24 @@ public class TickTackToeTest {
       org.junit.runner.JUnitCore.main("java.ticktacktoe.TickTackToeTest");
     }
 
-   @Test
-   public void test_initial_fill()
-   {
-     TickTackToe tick = new TickTackToe();
-     for (int i = 0; i <= 8; i++)
-     {
-       assertEquals(tick.board[i / 3][i % 3],(char) (i+49));
-     }
-   }
-   
+    @Test
+    public void test_initial_fill()
+    {
+      TickTackToe tick = new TickTackToe();
+      for (int i = 0; i <= 8; i++)
+      {
+        assertEquals(tick.board[i / 3][i % 3],(char) (i+49));
+      }
+    }
+
+    @Test
+    public void test_modify_board()
+    {
+      TickTackToe tick = new TickTackToe();
+      tick.modify_board(0,'1');
+      assertEquals('X', tick.board[0][0]);
+
+      tick.modify_board(1,'2');
+      assertEquals('O', tick.board[0][1]);
+    }
 }
